d5bf535d2007c27cbf3583d692c445fb4683478e
wolf-dagger <73078569+wolf-dagger@users.noreply.github.com>
2026-03-13 01:32:07 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-mono", jetbrainsMono.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,17 @@ export default function Home() {
         <main className="flex-1">
           {/* Hero section */}
           <section className="container mx-auto p-4 py-32">
-            <div>
-              <h1 className="text-black">
+            <div className="mx-auto max-w-4xl text-center">
+              <h1 className="text-black mb-6 text-6xl font-bold">
                 A better way to track your job applications...
               </h1>
-              <p className="text-gray-700">
+              <p className="text-muted-foreground mb-10 text-xl">
                 Capture, Organize and manage your job applications in one place.
               </p>
+              <div className="flex flex-col items-center gap-4">
+                <button>Start for free</button>
+                <p>Free forever. No payment needed...</p>
+              </div>
             </div>
           </section>
         </main>
