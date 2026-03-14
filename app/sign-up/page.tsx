5d079fb4e016e2eb0bee1cd096a9361e7f7b72ae
wolf-dagger <73078569+wolf-dagger@users.noreply.github.com>
2026-03-14 23:35:06 +0530

    feat: initial commit

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4"></div>
+    </>
+  );
 };
 
 export default SignUp;
